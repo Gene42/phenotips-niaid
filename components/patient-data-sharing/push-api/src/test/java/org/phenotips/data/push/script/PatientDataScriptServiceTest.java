c3bb7eeda291e100c41a2ceb9a0e678ce826832b
Sergiu Dumitriu <sergiu@phenotips.org>
2014-01-18 01:34:08 -0500

    Issue #675: Sharing patients with other PhenoTips instances Stub implementation for the Push code.

---
+++ components/patient-data-sharing/push-api/src/test/java/org/phenotips/data/push/script/PatientDataScriptServiceTest.java
@@ -0,0 +1,52 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.phenotips.data.push.script;
+
+import org.phenotips.data.push.PushPatientData;
+
+import org.xwiki.component.manager.ComponentLookupException;
+import org.xwiki.test.mockito.MockitoComponentMockingRule;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.mockito.Mockito.when;
+
+/**
+ * Tests for the {@link PushPatientDataScriptService} component.
+ * 
+ * @version $Id$
+ * @since 1.0M11
+ */
+public class PatientDataScriptServiceTest
+{
+    @Rule
+    public final MockitoComponentMockingRule<PushPatientDataScriptService> mocker =
+        new MockitoComponentMockingRule<PushPatientDataScriptService>(PushPatientDataScriptService.class);
+
+    @Test
+    public void sendPatientForwardsCalls() throws ComponentLookupException
+    {
+        PushPatientData component = this.mocker.getInstance(PushPatientData.class);
+        when(component.sendPatient(null, "nowhere")).thenReturn(-1);
+        Assert.assertEquals(-1, this.mocker.getComponentUnderTest().sendPatient(null, "nowhere"));
+    }
+}

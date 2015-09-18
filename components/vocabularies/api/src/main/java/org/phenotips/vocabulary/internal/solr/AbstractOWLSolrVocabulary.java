961dc1d3f6afc4b66f667117be2b052b58a6f0a9
Denys Pavlov <denys.pavlov@gmail.com>
2015-09-18 19:25:55 -0400

    added OWL parser to parse Orphanet to Solr

---
+++ components/vocabularies/api/src/main/java/org/phenotips/vocabulary/internal/solr/AbstractOWLSolrVocabulary.java
@@ -0,0 +1,185 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+package org.phenotips.vocabulary.internal.solr;
+
+import org.phenotips.vocabulary.VocabularyTerm;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+import org.apache.solr.common.SolrException;
+import org.apache.solr.common.SolrInputDocument;
+
+import com.hp.hpl.jena.ontology.OntClass;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+/**
+ * Ontologies processed from OWL files share much of the processing code.
+ *
+ * @version $Id$
+ * @since 1.2M4 (under different names since 1.1)
+ */
+public abstract class AbstractOWLSolrVocabulary extends AbstractSolrVocabulary
+{
+    protected static final String VERSION_FIELD_NAME = "version";
+
+
+    @Override
+    public VocabularyTerm getTerm(String id) {
+        VocabularyTerm result = super.getTerm(id);
+        if (result == null) {
+            Map<String, String> queryParameters = new HashMap<>();
+            queryParameters.put("id", id);
+            List<VocabularyTerm> results = search(queryParameters);
+            if (results != null && !results.isEmpty()) {
+                result = search(queryParameters).iterator().next();
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public int reindex(String sourceUrl) {
+        this.clear();
+        return this.index(sourceUrl);
+    }
+
+    /**
+     * Add a vocabulary to the index.
+     *
+     * @param sourceUrl the address from where to get the vocabulary source file
+     *
+     * @return {@code 0} if the indexing succeeded, {@code 1} if writing to the Solr server failed, {@code 2} if the
+     * specified URL is invalid
+     */
+    protected int index(String sourceUrl) {
+        // fetch the ontology. If this is over the network, it may take a while.
+        OntModel ontModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);
+        ontModel.read(sourceUrl);
+
+        // get the root classes of the ontology that we can start the parsing with.
+        Collection<OntClass> roots = getRootClasses(ontModel);
+        // reusing doc for speed (see http://wiki.apache.org/lucene-java/ImproveIndexingSpeed)
+        SolrInputDocument doc = new SolrInputDocument();
+        for (OntClass root : roots) {
+            addDoc(doc, root, roots);
+        }
+
+        try {
+            this.externalServicesAccess.getSolrConnection().commit(true, true);
+        } catch (SolrServerException e) {
+            e.printStackTrace();
+            return 1;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 1;
+        }
+
+        return 0;
+    }
+
+    private void addDoc(SolrInputDocument doc, OntClass ontClass, Collection<OntClass> roots) {
+        this.parseSolrDocumentFromOntClass(doc, ontClass, roots);
+
+        try {
+            this.externalServicesAccess.getSolrConnection().add(doc);
+            doc.clear();
+        } catch (SolrServerException ex) {
+            this.logger.warn("Failed to index ontology: {}", ex.getMessage());
+            return;
+        } catch (IOException ex) {
+            this.logger.warn("Failed to communicate with the Solr server while indexing ontology: {}",
+                    ex.getMessage());
+            return;
+        } catch (OutOfMemoryError ex) {
+            this.logger.warn("Failed to add terms to the Solr. Ran out of memory. {}", ex.getMessage());
+            return;
+        }
+
+
+        // extract any subclasses, add our class to the roots, and recurse.
+        // this shouldn't lead us to walk circles in the ontology, since subclasses are implicit.
+        Collection<OntClass> newRoots = new HashSet<>(roots);
+        newRoots.add(ontClass);
+        for (OntClass subClass : ontClass.listSubClasses().toSet()) {
+            addDoc(doc, subClass, newRoots);
+        }
+    }
+
+    protected abstract Collection<OntClass> getRootClasses(OntModel ontModel);
+
+    protected abstract SolrInputDocument parseSolrDocumentFromOntClass(SolrInputDocument doc,
+                                                                       OntClass ontClass,
+                                                                       Collection<OntClass> roots);
+
+    /**
+     * Delete all the data in the Solr index.
+     *
+     * @return {@code 0} if the command was successful, {@code 1} otherwise
+     */
+    protected int clear() {
+        try {
+            this.externalServicesAccess.getSolrConnection().deleteByQuery("*:*");
+            return 0;
+        } catch (SolrServerException ex) {
+            this.logger.error("SolrServerException while clearing the Solr index", ex);
+        } catch (IOException ex) {
+            this.logger.error("IOException while clearing the Solr index", ex);
+        }
+        return 1;
+    }
+
+    @Override
+    public String getVersion() {
+        QueryResponse response;
+        SolrQuery query = new SolrQuery();
+        SolrDocumentList termList;
+        SolrDocument firstDoc;
+
+        query.setQuery("version:*");
+        query.set("rows", "1");
+        try {
+            response = this.externalServicesAccess.getSolrConnection().query(query);
+            termList = response.getResults();
+
+            if (!termList.isEmpty()) {
+                firstDoc = termList.get(0);
+                return firstDoc.getFieldValue(VERSION_FIELD_NAME).toString();
+            }
+        } catch (SolrServerException | SolrException | IOException ex) {
+            this.logger.warn("Failed to query ontology version: {}", ex.getMessage());
+        }
+        return null;
+    }
+
+    /** The number of documents to be added and committed to Solr at a time. */
+    protected int getSolrDocsPerBatch() {
+        return 0;
+    }
+}
--- components/vocabularies/orphanet/api/src/main/java/org/phenotips/vocabulary/internal/solr/Orphanet.java
+++ components/vocabularies/orphanet/api/src/main/java/org/phenotips/vocabulary/internal/solr/Orphanet.java
@@ -21,13 +21,24 @@ import org.phenotips.vocabulary.VocabularyTerm;
 
 import org.xwiki.component.annotation.Component;
 
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.solr.common.SolrInputDocument;
+
+import com.hp.hpl.jena.ontology.OntClass;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.Restriction;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
 
 /**
  * Provides access to the ORPHANET ontology. The ontology prefix is {@code ORPHANET}.
@@ -38,23 +49,27 @@ import org.apache.commons.lang3.StringUtils;
 @Component
 @Named("orphanet")
 @Singleton
-public class Orphanet extends AbstractSolrVocabulary
+public class Orphanet extends AbstractOWLSolrVocabulary
 {
     @Override
-    protected String getName()
-    {
+    protected String getName() {
         return "ORPHANET";
     }
 
     @Override
-    public String getDefaultSourceLocation()
-    {
-        return "http://data.bioontology.org/ontologies/ORDO/submissions/7/download?apikey=8b5b7825-538d-40e0-9e9e-5ab9274a9aeb&download_format=owl";
+    protected int getSolrDocsPerBatch() {
+        return 15000;
     }
 
     @Override
-    public VocabularyTerm getTerm(String id)
-    {
+    public String getDefaultSourceLocation() {
+//        return
+// "http://data.bioontology.org/ontologies/ORDO/submissions/7/download?apikey=8b5b7825-538d-40e0-9e9e-5ab9274a9aeb";
+        return "file:///home/meatcar/Downloads/ordo_orphanet.owl";
+    }
+
+    @Override
+    public VocabularyTerm getTerm(String id) {
         VocabularyTerm result = super.getTerm(id);
         if (result == null) {
             String optionalPrefix = this.getName() + ":";
@@ -66,11 +81,135 @@ public class Orphanet extends AbstractSolrVocabulary
     }
 
     @Override
-    public Set<String> getAliases()
-    {
+    protected Collection<OntClass> getRootClasses(OntModel ontModel) {
+        Collection<OntClass> keepers = new HashSet<>();
+        List<OntClass> ontClasses = ontModel.listClasses().toList();
+        for (OntClass ontClass : ontClasses) {
+            if ("disease".equals(ontClass.getLabel(null))) {
+                keepers.add(ontClass);
+            } else if ("group of disorders".equals(ontClass.getLabel(null))) {
+                keepers.add(ontClass);
+            } else if ("genetic material".equals(ontClass.getLabel(null))) {
+                keepers.add(ontClass);
+            }
+        }
+        return keepers;
+    }
+
+    @Override
+    public Set<String> getAliases() {
         Set<String> result = new HashSet<String>();
         result.add(getName());
         return result;
+    }
+
+    @Override
+    protected SolrInputDocument parseSolrDocumentFromOntClass(SolrInputDocument doc, OntClass ontClass,
+                                                              Collection<OntClass> roots) {
+        // orphanet label == phenotips vocab name
+        doc.addField("id", getOrphanetId(ontClass.getLocalName()));
+        doc.addField("name", ontClass.getLabel(null));
+
+        // save all parents
+        List<OntClass> parents = ontClass.listSuperClasses().toList();
+        for (OntClass parent : parents) {
+            if (parent.isRestriction() && parent.asRestriction().isSomeValuesFromRestriction()) {
+                // a restriction, we only care about "SomeValuesFrom" restrictions
+                // get get the value of the restriction, make sure it's under the roots we care about, not some other
+                // property.
+                // TODO in another iteration, later:
+                //   epidemiology (1/2000000, family, etc)
+                //   prevalence
+                Restriction restriction = parent.asRestriction();
+                OntClass value = restriction.asSomeValuesFromRestriction().getSomeValuesFrom().as(OntClass.class);
+                for (OntClass root : roots) {
+                    if (value.hasSuperClass(root)) {
+                        doc.addField("term_category", getOrphanetId(value.getLocalName()));
+                    }
+                }
+            } else if (isRegularClass(parent)) {
+                // just a regular clas:s.
+                doc.addField("term_category", getOrphanetId(parent.getLocalName()));
+                // we should be able to use the {{@link OntClass.getSuperClass()}} method, but since it returns an
+                // **arbitrary** selection, we'll just pick the first superclass that's actually a class
+                // that we stumble upon.
+                if (doc.getField("is_a") == null) {
+                    doc.addField("is_a", getOrphanetId(parent.getLocalName()));
+                }
+            }
+        }
+
+        extractProperties(doc, ontClass);
+        return doc;
+    }
+
+    private boolean isRegularClass(OntClass ontClass) {
+        return !(ontClass.isRestriction()
+                || ontClass.isEnumeratedClass()
+                || ontClass.isUnionClass()
+                || ontClass.isIntersectionClass()
+                || ontClass.isComplementClass());
+    }
+
+    private void extractProperties(SolrInputDocument doc, OntClass ontClass) {
+        List<Statement> statements = ontClass.listProperties().toList();
+        for (Statement statement : statements) {
+            Resource subject = statement.getSubject();
+            RDFNode object = statement.getObject();
+            Property predicate = statement.getPredicate();
+
+            if (!subject.as(OntClass.class).equals(ontClass)) {
+                throw new RuntimeException("Subject is not the disease.");
+            }
+
+            String relation = predicate.getLocalName();
+            switch (relation) {
+                case "hasDbXref":
+                    extractDBXref(doc, object);
+                    break;
+                case "alternative_term":
+                    // pull alternative term
+                    doc.addField("alternative_term", object.asLiteral().getString());
+                    break;
+                case "definition":
+                    // pull definition term
+                    doc.addField("def", object.asLiteral().getString());
+                    break;
+                case "symbol":
+                    // pull gene symbol (only genes have this.)
+                    doc.addField("gene_symbol", object.asLiteral().getString());
+                    break;
+                default:
+            }
+        }
+    }
+
+    private void extractDBXref(SolrInputDocument doc, RDFNode object) {
+        // pull reference to other dbs (OMIM, HGNC, Ensembl)
+        // stick external db id into solr
+        String[] parts = object.asLiteral().getLexicalForm().split(":");
+        String id = parts[1];
+        String ontology = parts[0];
+        switch (ontology) {
+            case "OMIM":
+            case "HGNC":
+            case "Ensembl":
+                doc.addField(ontology.toLowerCase() + "_id", id);
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * Get a numerical id string from a localName. Assuming the localName is in the form "Orphanet_XXX"
+     *
+     * @param localName the localName of an OWL class.
+     *
+     * @return the string id.
+     */
+    private String getOrphanetId(String localName) {
+        return localName.replace("Orphanet_", "");
     }
 
 }

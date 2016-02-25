Gene42/phenotips-niaid

commit 7f11be1d1ad5ed795a8c29577e9ab361b54a3df1

PT-1951 Patient measurements - UI improvements add armspan measurement

components/patient-measurements/resources/src/main/resources/resources/uicomponents/phenotips/measurements.js
@@ -362,8 +362,10 @@ var PhenoTips = (function(PhenoTips) {
       this.el.select('.measurement-row').each((function(el) {
         if (el.hasClassName('computed')) {
           this._rows.push(new widgets.ComputedMeasurementSetRow(el, this));
+        } else if (el.down('[name$=type]').value == 'armspan') {
+          this._rows.push(new widgets.ArmspanMeasurementSetRow(el, this));
         } else {
-          this._rows.push(new widgets.MeasurementSetRow(el, this));
+          this._rows.push(new widgets.PercentileSDMeasurementSetRow(el, this));
         }
       }).bind(this));
 
@@ -566,20 +568,50 @@ var PhenoTips = (function(PhenoTips) {
       this.el = el; this.parent = parent;
 
       // Bind methods
-      this.fetchAndRenderPercentileSd = this.fetchAndRenderPercentileSd.bind(this);
       this.destroy = this.destroy.bind(this);
-      this._genderChangeHandler = this._genderChangeHandler.bind(this);
       this.getMeasurementType = this.getMeasurementType.bind(this);
       this.getValue = this.getValue.bind(this);
+
+      if (XWiki.contextaction == 'edit') {
+        // DOM shortcuts
+        this._valueEl = this.el.select('[name$=value]')[0];
+      }
+    },
+
+    getMeasurementType: function() {
+      return this.el.down('[name$=type]').value;
+    },
+
+    getValue: function() {
+      if (XWiki.contextaction == 'edit') {
+        return this._valueEl.value;
+      } else {
+        return this.el.down('span.val').innerHTML;
+      }
+    },
+
+    destroy: function() {
+      this._selectAssocPhenotypes([]);
+
+      $$('input[name$=gender]').each((function(el) {
+        el.stopObserving('click', this._genderChangeHandler);
+      }).bind(this));
+    }
+  });
+
+  widgets.PercentileSDMeasurementSetRow = Class.create(widgets.MeasurementSetRow, {
+    initialize: function($super, el, parent) {
+      $super(el, parent);
+
+      // Bind methods
+      this.fetchAndRenderPercentileSd = this.fetchAndRenderPercentileSd.bind(this);
+      this._genderChangeHandler = this._genderChangeHandler.bind(this);
       this._selectAssocPhenotypes = this._selectAssocPhenotypes.bind(this);
       this._selectMeasurementTerm = this._selectMeasurementTerm.bind(this);
       this._unselectMeasurementTerm = this._unselectMeasurementTerm.bind(this);
       this._selectFormElementForPhenotype = this._selectFormElementForPhenotype.bind(this);
 
       if (XWiki.contextaction == 'edit') {
-        // DOM shortcuts
-        this._valueEl = this.el.select('[name$=value]')[0];
-
         // Init array for smart phenotype terms triggered by this row
         this._curSelectedTerms = [];
 
@@ -599,20 +631,8 @@ var PhenoTips = (function(PhenoTips) {
       }
     },
 
-    getMeasurementType: function() {
-      return this.el.down('[name$=type]').value;
-    },
-
-    getValue: function() {
-      if (XWiki.contextaction == 'edit') {
-        return this._valueEl.value;
-      } else {
-        return this.el.down('span.val').innerHTML;
-      }
-    },
-
     fetchAndRenderPercentileSd: function(e) {
-      var pctlEl = this.el.select('.pctl')[0];
+      var pctlEl = this.el.select('.feedback')[0];
       var fetchParams = {
         'measurement': this.el.select('[name$=type]')[0].value,
         'value': this._valueEl.value,
@@ -751,17 +771,45 @@ var PhenoTips = (function(PhenoTips) {
         return existingElement;
       }
     },
-
-    destroy: function() {
-      this._selectAssocPhenotypes([]);
-
-      $$('input[name$=gender]').each((function(el) {
-        el.stopObserving('click', this._genderChangeHandler);
-      }).bind(this));
-    }
   });
 
-  widgets.ComputedMeasurementSetRow = Class.create(widgets.MeasurementSetRow, {
+  widgets.ArmspanMeasurementSetRow = Class.create(widgets.MeasurementSetRow, {
+    initialize: function($super, el, parent) {
+      $super(el, parent);
+
+      // DOM node shortcuts
+      this._heightInputEl = this.parent.el.select('input[name$=type][value=height]')[0].up(0).select('input[name$=value]')[0];
+      this._feedbackEl = this.el.select('.feedback')[0];
+
+      // Bind methods
+      this._renderFeedback = this._renderFeedback.bind(this);
+
+      if (XWiki.contextaction == 'edit') {
+        // Attach handlers
+        [this._heightInputEl, this._valueEl].each((function(el) {
+          el.observe('input', this._renderFeedback);
+        }).bind(this));
+
+        this._renderFeedback();
+      }
+    },
+
+    _renderFeedback: function() {
+      if (this.getValue() && !isNaN(this.getValue()) && this._heightInputEl.value && !isNaN(this._heightInputEl.value)) {
+        var delta = this.getValue() - this._heightInputEl.value;
+        var feedback = "= $services.localization.render('PhenoTips.MeasurementsClass_height')";
+        feedback += delta > 0 ? ' + ' : ' &minus; ';
+        feedback += Math.abs(delta);
+        feedback += "$services.localization.render('phenotips.UIXField.measurements.units.cm')"; 
+
+        this._feedbackEl.innerHTML = feedback;
+      } else {
+        this._feedbackEl.innerHTML = '';
+      }
+    },
+  });
+
+  widgets.ComputedMeasurementSetRow = Class.create(widgets.PercentileSDMeasurementSetRow, {
     initialize: function($super, el, parent) {
       $super(el, parent);
 

components/navigation/ui/src/main/resources/data/WebHome.xml
@@ -392,7 +392,7 @@ $!value
         <number>3</number>
         <picker>0</picker>
         <prettyName>ordering</prettyName>
-        <relationalStorage>0</relationalStorage>
+        <relationalStorage>1</relationalStorage>
         <separator> </separator>
         <separators>|, </separators>
         <size>1</size>

components/patient-data/impl/src/main/resources/ApplicationResources.properties
@@ -286,6 +286,7 @@ phenotips.UIXField.measurements.types.weight=Weight
 phenotips.UIXField.measurements.types.height=Height
 phenotips.UIXField.measurements.types.bmi=BMI
 phenotips.UIXField.measurements.types.sitting=Sitting Height
+phenotips.UIXField.measurements.types.armspan=Armspan
 phenotips.UIXField.measurements.types.hc=Head Circumference
 phenotips.UIXField.measurements.types.philtrum=Philtrum length
 phenotips.UIXField.measurements.types.ear=Ear Length

components/patient-measurements/resources/src/main/resources/resources/uicomponents/phenotips/measurements.css
@@ -92,7 +92,7 @@
 .measurement-row span.unit {
   width: 60px;
 }
-.measurement-row span.pctl {
+.measurement-row span.feedback {
   font-size: 80%;
   width: 120px;
 }

commit 9885da955694f19c6c66eb6f82332459210f6a44

PT-1951 Patient measurements - UI improvements fix regression introduced in 7f11be1d1ad5ed795a8c29577e9ab361b54a3df1

components/patient-measurements/resources/src/main/resources/resources/uicomponents/phenotips/measurements.js
@@ -768,6 +768,12 @@ var PhenoTips = (function(PhenoTips) {
         return existingElement;
       }
     },
+
+    destroy: function($super) {
+      this._selectAssocPhenotypes([]);
+
+      $super();
+    }
   });
 
   widgets.ArmspanMeasurementSetRow = Class.create(widgets.MeasurementSetRow, {
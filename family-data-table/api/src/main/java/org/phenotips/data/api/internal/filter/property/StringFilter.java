028e69d131bec7bc398247c223b3fec0afac5114
Sebastian Stanisor <sebialex@gmail.com>
2016-10-24 22:18:00 -0400

    added document property handling to StringFilter

--- family-data-table/api/src/main/java/org/phenotips/data/api/internal/filter/property/StringFilter.java
+++ family-data-table/api/src/main/java/org/phenotips/data/api/internal/filter/property/StringFilter.java
@@ -12,7 +12,6 @@ import org.phenotips.data.api.internal.filter.DocumentQuery;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -29,10 +28,29 @@ import com.xpn.xwiki.objects.classes.BaseClass;
  */
 public class StringFilter extends AbstractPropertyFilter<String>
 {
+    /** Filter param key. */
     public static final String MATCH_KEY = "match";
+
+    /** Match value. */
+    public static final String MATCH_EXACT = "exact";
+
+    /** Match value. */
+    public static final String MATCH_SUBSTRING = null;
+
+    /** Match value. */
+    public static final String MATCH_CASE_INSENSITIVE = "ci";
+
+    private static final String DOC_CREATOR_PROPERTY = "creator";
+
+    private static final String DOC_AUTHOR_PROPERTY = "author";
 
     private String match;
 
+    /**
+     * Constructor.
+     * @param property PropertyInterface
+     * @param baseClass BaseClass
+     */
     public StringFilter(PropertyInterface property, BaseClass baseClass)
     {
         super(property, baseClass);
@@ -56,6 +74,9 @@ public class StringFilter extends AbstractPropertyFilter<String>
         if (valueObj instanceof JSONArray) {
             JSONArray valuesArray = (JSONArray) valueObj;
             for (Object objValue : valuesArray) {
+                if (objValue == null) {
+                    continue;
+                }
                 this.values.add(String.valueOf(objValue));
             }
         } else if (valueObj instanceof String) {
@@ -80,32 +101,67 @@ public class StringFilter extends AbstractPropertyFilter<String>
 
         if (super.isDocumentProperty) {
             objPropName = "str(" +  super.getDocumentPropertyName() + ")";
-        }
-        else {
+        } else {
             objPropName = super.getObjectPropertyName() + ".value";
         }
 
-        where.append(" and ");
+        where.append(" and (");
 
-        if (this.values.size() > 1) {
-            where.append(objPropName).append(" in (").append(StringUtils.repeat("?", ", ", this.values.size()));
-            where.append(") ");
-            bindingValues.addAll(this.values);
-        } else {
-            String value = this.values.get(0);
+        for (int i = 0, len = this.values.size(); i < len; i++) {
+            String value = this.values.get(i);
+            if (value == null) {
+                continue;
+            }
 
-            if (StringUtils.equals(this.match, "exact")) {
-                where.append(objPropName).append("=? ");
-                bindingValues.add(value);
-            } else if (StringUtils.equals(this.match, "ci")) {
-                where.append("upper(").append(objPropName).append(")=? ");
-                bindingValues.add(StringUtils.upperCase(value));
+            if (i > 0) {
+                where.append(" or ");
+            }
+
+            if (super.isDocumentProperty) {
+                boolean docAuthorOrCreator = StringUtils.equals(super.documentPropertyName, DOC_CREATOR_PROPERTY)
+                                          || StringUtils.equals(super.documentPropertyName, DOC_AUTHOR_PROPERTY);
+
+                this.handleDocumentProperties(where, bindingValues, objPropName, value, docAuthorOrCreator);
             } else {
-                where.append("upper(").append(objPropName).append(") like upper(?) ESCAPE '!' ");
-                bindingValues.add("%" + value.replaceAll("[\\[_%!]", "!$0") + "%");
+                this.handleMatch(where, bindingValues, objPropName, value, this.match);
             }
         }
 
-        return where;
+        return where.append(") ");
+    }
+
+    private void handleDocumentProperties(StringBuilder where, List<Object> bindingValues, String propName, String
+        value, boolean docAuthorOrCreator)
+    {
+        String docPropMatch = MATCH_SUBSTRING;
+        String docPropName = propName;
+        String docPropValue = value;
+
+        if (docAuthorOrCreator && StringUtils.startsWith(value, "XWiki.")) {
+            docPropMatch = MATCH_EXACT;
+        } else if (docAuthorOrCreator && StringUtils.contains(value, ":")) {
+            docPropMatch = MATCH_EXACT;
+            docPropValue = StringUtils.substringAfter(value, ":");
+        } else {
+            docPropName = StringUtils.replace(propName, "_", ".");
+        }
+
+        this.handleMatch(where, bindingValues, docPropName, docPropValue, docPropMatch);
+    }
+
+    private void handleMatch(StringBuilder where, List<Object> bindingValues, String propName, String value, String
+        match) {
+
+        if (StringUtils.equals(match, MATCH_EXACT)) {
+            where.append(propName).append("=? ");
+            bindingValues.add(value);
+        } else if (StringUtils.equals(match, MATCH_CASE_INSENSITIVE)) {
+            where.append("upper(").append(propName).append(")=? ");
+            bindingValues.add(StringUtils.upperCase(value));
+        } else {
+            where.append("upper(").append(propName).append(") like upper(?) ESCAPE '!' ");
+            bindingValues.add("%" + value.replaceAll("[\\[_%!]", "!$0") + "%");
+        }
+
     }
 }

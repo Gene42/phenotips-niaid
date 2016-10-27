2e820daa7dc4ec93b5ec42181da3f2cf030599d8
danielpgross <daniel.gross.2@gmail.com>
2016-10-27 18:24:41 -0400

    NIAID-54 Add permission checking, error handling, comments to FG REST

--- family-groups/rest/src/main/java/org/phenotips/data/rest/FamilyGroupFamilies.java
+++ family-groups/rest/src/main/java/org/phenotips/data/rest/FamilyGroupFamilies.java
@@ -19,16 +19,33 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
+ * Allows for adding and removing Families in Family Groups, via REST.
+ *
  * @version $Id$
  */
 @Path("/family-groups/{family-group-id}/families")
 public interface FamilyGroupFamilies extends XWikiRestComponent
 {
+    /**
+     * Adds the specified Family to the specified Family Group, returning JSON representing the updated Family Group.
+     *
+     * @param familyGroupId the Family Group ID.
+     * @param json the JSON request body.
+     * @return JSON representing the updated Family Group.
+     */
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     Response addFamilyToFamilyGroup(@PathParam("family-group-id") String familyGroupId, String json);
 
+    /**
+     * Removes the specified Family from the specified Family Group, returning JSON representing the updated Family
+     * Group.
+     *
+     * @param familyGroupId the Family Group ID.
+     * @param familyId the Family ID.
+     * @return JSON representing the updated Family Group.
+     */
     @DELETE
     @Path("/{family-id}")
     @Produces(MediaType.APPLICATION_JSON)
--- family-groups/rest/src/main/java/org/phenotips/data/rest/internal/DefaultFamilyGroupFamilies.java
+++ family-groups/rest/src/main/java/org/phenotips/data/rest/internal/DefaultFamilyGroupFamilies.java
@@ -12,8 +12,13 @@ import org.phenotips.entities.PrimaryEntityGroupManager;
 import org.phenotips.entities.PrimaryEntityManager;
 import org.phenotips.familyGroups.Family;
 import org.phenotips.familyGroups.FamilyGroup;
+import org.phenotips.security.authorization.AuthorizationService;
 
 import org.xwiki.component.annotation.Component;
+import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.security.authorization.Right;
+import org.xwiki.users.User;
+import org.xwiki.users.UserManager;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -24,6 +29,8 @@ import javax.ws.rs.core.Response;
 import org.json.JSONObject;
 
 /**
+ * Default implementation of Families in Family Groups REST endpoint, using the Entities API.
+ *
  * @version $Id$
  */
 @Component
@@ -43,14 +50,32 @@ public class DefaultFamilyGroupFamilies implements FamilyGroupFamilies
     @Named("FamilyGroup:Family")
     private PrimaryEntityGroupManager<FamilyGroup, Family> familiesInFamilyGroupManager;
 
+    @Inject
+    private AuthorizationService authorizationService;
+
+    @Inject
+    private UserManager users;
+
     @Override
     public Response addFamilyToFamilyGroup(String familyGroupId, String json)
     {
         JSONObject jsonObj = new JSONObject(json);
+
         FamilyGroup familyGroup = (FamilyGroup) familyGroupManager.get(familyGroupId);
         Family family = (Family) familyManager.get(jsonObj.optString("familyId"));
-        familiesInFamilyGroupManager.addMember(familyGroup, family);
-        return Response.ok(familyGroup.toJSON(), MediaType.APPLICATION_JSON).build();
+
+        Response resp = checkInputs(familyGroup, family);
+        if (resp != null) {
+            return resp;
+        }
+
+        boolean success = familiesInFamilyGroupManager.addMember(familyGroup, family);
+        if (success) {
+            return Response.ok(familyGroup.toJSON(), MediaType.APPLICATION_JSON).build();
+        } else {
+            return generateErrorResponse("The Family was not added to the Family Group due to an error.",
+                Response.Status.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @Override
@@ -58,23 +83,78 @@ public class DefaultFamilyGroupFamilies implements FamilyGroupFamilies
     {
         FamilyGroup familyGroup = (FamilyGroup) familyGroupManager.get(familyGroupId);
         Family family = (Family) familyManager.get(familyId);
-        familiesInFamilyGroupManager.removeMember(familyGroup, family);
-        return Response.ok(familyGroup.toJSON(), MediaType.APPLICATION_JSON).build();
+
+        Response resp = checkInputs(familyGroup, family);
+        if (resp != null) {
+            return resp;
+        }
+
+        boolean success = familiesInFamilyGroupManager.removeMember(familyGroup, family);
+
+        if (success) {
+            return Response.ok(familyGroup.toJSON(), MediaType.APPLICATION_JSON).build();
+        } else {
+            return generateErrorResponse("The Family was not removed from the Family Group due to an error.",
+                Response.Status.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Checks the endpoint inputs for errors, returning an appropriate response in case an error is found. Checks for
+     * existence (non-null) on both inputs, as well as user permissions to modify the Family Group.
+     *
+     * @param familyGroup the Family Group endpoint input.
+     * @param family the Family endpoint input.
+     * @return null, if no errors are found.
+     *         an appropriate response, in case an error is found.
+     */
+    private Response checkInputs(FamilyGroup familyGroup, Family family)
+    {
+        if (familyGroup == null) {
+            return generateErrorResponse("Family Group not found.", Response.Status.NOT_FOUND);
+        }
+        if (family == null) {
+            return generateErrorResponse("Family not found.", Response.Status.NOT_FOUND);
+        }
+
+        if (!curUserHasEditPermission(familyGroup)) {
+            return generateErrorResponse("User lacks permission to add or remove Families for this Family Group.",
+                Response.Status.FORBIDDEN);
+        }
+        return null;
+    }
+
+    /**
+     * Returns whether the current user has edit permission the given Family Group.
+     *
+     * @param familyGroup the Family Group to check.
+     * @return true if the current user has edit permission.
+     *         false if the current user doesn't have edit permission.
+     */
+    private boolean curUserHasEditPermission(FamilyGroup familyGroup)
+    {
+        User user = this.users.getCurrentUser();
+        DocumentReference familyGroupRef = familyGroup.getDocumentReference();
+        return this.authorizationService.hasAccess(user, Right.EDIT, familyGroupRef);
     }
 
     /**
      * Generates an error response.
      *
-     * @param errorText the plain text of the response.
+     * @param errorText the text of the response, which will be returned as the value for an "error" key in a JSON
+     *                  object.
      * @param status the HTTP status code of the response.
      * @return the Response object.
      */
     private Response generateErrorResponse(String errorText, Response.Status status)
     {
+        JSONObject jsonObj = new JSONObject();
+        jsonObj.put("error", errorText);
+
         return Response
             .status(status)
-            .entity(errorText)
-            .type(MediaType.TEXT_PLAIN)
+            .entity(jsonObj)
+            .type(MediaType.APPLICATION_JSON)
             .build();
     }
 }

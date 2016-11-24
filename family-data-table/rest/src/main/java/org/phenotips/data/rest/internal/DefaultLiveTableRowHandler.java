9fcae5fdda9cbf4ec1f404cd70b553037d824c15
Sebastian Stanisor <sebialex@gmail.com>
2016-11-23 20:00:56 -0500

    family data table update - fixed rights check in the table row handler - added ownership filter to the family table - fixed my families gadget (rendering was broken due to a missing newline)

--- family-data-table/rest/src/main/java/org/phenotips/data/rest/internal/DefaultLiveTableRowHandler.java
+++ family-data-table/rest/src/main/java/org/phenotips/data/rest/internal/DefaultLiveTableRowHandler.java
@@ -14,6 +14,7 @@ import org.xwiki.component.annotation.Component;
 import org.xwiki.component.manager.ComponentManager;
 import org.xwiki.model.reference.DocumentReference;
 import org.xwiki.model.reference.DocumentReferenceResolver;
+import org.xwiki.security.authorization.AccessDeniedException;
 import org.xwiki.security.authorization.AuthorizationManager;
 import org.xwiki.security.authorization.Right;
 
@@ -77,7 +78,13 @@ public class DefaultLiveTableRowHandler implements LiveTableRowHandler
 
         XWiki wiki = context.getWiki();
 
-        boolean viewable = this.access.hasAccess(Right.VIEW, context.getUserReference(), docRef);
+        boolean viewable;
+        try {
+            this.access.checkAccess(Right.VIEW, context.getUserReference(), docRef);
+            viewable = true;
+        } catch (AccessDeniedException e) {
+            viewable = false;
+        }
 
         row.put("doc_viewable", viewable);
 
